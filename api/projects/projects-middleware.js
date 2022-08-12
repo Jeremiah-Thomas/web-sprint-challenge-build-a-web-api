4a51eed9edf3988ba0bf29b859e07d089753aa26
Jeremiah Thomas <jeremiah.b.thomas12@gmail.com>
2022-08-12 00:02:48 -0400

    All /api/projects endpoints are working

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,42 @@
 // add middlewares here related to projects
+
+const Projects = require("./projects-model");
+
+const validateProjectId = (req, res, next) => {
+  Projects.get(req.params.id).then((project) => {
+    if (project == null) {
+      res
+        .status(404)
+        .json({ message: `No project exists with id ${req.params.id}` });
+    } else {
+      req.project = project;
+      next();
+    }
+  });
+};
+
+const validateProject = (req, res, next) => {
+  if (req.method === "POST") {
+    if (req.body.name == null || req.body.description == null) {
+      res
+        .status(400)
+        .json({ message: "project must include name and description." });
+    } else {
+      next();
+    }
+  } else if (req.method === "PUT") {
+    if (
+      req.body.name == null ||
+      req.body.description == null ||
+      req.body.completed == null
+    ) {
+      res.status(400).json({
+        message: "project must include name, description, and completed.",
+      });
+    } else {
+      next();
+    }
+  }
+};
+
+module.exports = { validateProjectId, validateProject };
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,46 @@
 // Write your "projects" router here!
+const express = require("express");
+
+const Projects = require("./projects-model");
+const Actions = require("./../actions/actions-model");
+const { validateProjectId, validateProject } = require("./projects-middleware");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Projects.get().then((projects) => {
+    res.json(projects);
+  });
+});
+
+router.get("/:id", validateProjectId, (req, res) => {
+  res.json(req.project);
+});
+
+router.post("/", validateProject, (req, res) => {
+  Projects.insert(req.body).then((project) => {
+    res.json(project);
+  });
+});
+
+router.put("/:id", validateProjectId, validateProject, (req, res) => {
+  Projects.update(req.params.id, req.body).then((project) => {
+    res.json(project);
+  });
+});
+
+router.delete("/:id", validateProjectId, (req, res) => {
+  Projects.remove(req.params.id).then((num) => res.send("Project deleted."));
+});
+
+router.get("/:id/actions", validateProjectId, (req, res) => {
+  Actions.get().then((actions) => {
+    res.json(
+      actions.filter((action) => {
+        return action.project_id === parseInt(req.params.id);
+      })
+    );
+  });
+});
+
+module.exports = router;
